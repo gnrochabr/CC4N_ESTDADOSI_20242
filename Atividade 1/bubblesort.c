c6c4630d9f06d6219f2d03ad51785943cb1b7989
Giovanni Persio Gonçalves <68757820+Giovannipersio@users.noreply.github.com>
2024-11-02 18:52:43 +0000

    Send pull

--- Atividade 1/bubblesort.c
+++ Atividade 1/bubblesort.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
 
+// Code to make bubble sort ordenation
+
+void bubble_sort (int *vetor, int tam);
+
+int main (void)
+{
+    int vetor[10] = {5, 3, 2, 7, 6, 8, 1, 4, 0, 9};
+    bubble_sort(vetor, 10);
+
+    return 0;
+}
+
+void bubble_sort (int *vetor, int tam)
+{
+    for (int i = 0; i < tam; i++)
+    {
+        for (int j = 0; j < tam - 1; j++)
+        {
+            if (vetor[j] > vetor[j + 1])
+            {
+                int aux = vetor[j];
+                vetor[j] = vetor[j + 1];
+                vetor[j + 1] = aux;
+            }
+        }
+    }
+    for (int i = 0; i < tam; i++)
+    {
+        printf("%d ", vetor[i]);    
+    }
+    
+}
--- Atividade 1/selectionsort.c
+++ Atividade 1/selectionsort.c
@@ -1,1 +1,46 @@
+// This algorithm aims to create a selection sort
 
+#include <stdio.h>
+
+// Functions prototype
+void selection_sort (int *array, int array_len);
+
+// Main function
+int main (void)
+{
+    int array[] = {44, 6, 8, 1, 0, 2, 3, 4, 7, 9, 89, 21};
+    
+    // Calculate the array size
+    int array_len = sizeof(array) / sizeof(array[0]);
+
+    selection_sort(array, array_len);
+    printf("Array ordered: ");
+    for (int i = 0; i < array_len; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    
+    return 0;
+}
+
+// Selection sort function
+void selection_sort (int *array, int array_len)
+{
+    int max_idp = 0;
+    int step = 0;
+
+    for (int i = 0; i < array_len; i++)
+    {
+        max_idp = i;
+        for (int j = i + 1; j < array_len; j++)
+        {
+            if (array[j] < array[max_idp])
+            {
+                max_idp = j;
+            }
+        }
+        step = array[i];
+        array[i] = array[max_idp];
+        array[max_idp] = step;
+    }    
+}

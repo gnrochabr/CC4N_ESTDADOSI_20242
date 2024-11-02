303443c73eb6c5b8ffb806af33caabd837ba0465
Giovanni Persio Gonçalves <68757820+Giovannipersio@users.noreply.github.com>
2024-11-02 18:52:58 +0000

    send pull

--- Atividade 1/insetionsort.c
+++ Atividade 1/insetionsort.c
@@ -1,2 +1,39 @@
 // This code is for the insetion sort
 
+#include <stdio.h>
+
+// Function prototype
+void insertion_sort(int arr[], int n);
+
+int main(void)
+{
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    insertion_sort(arr, n);
+
+    printf("Sorted array: \n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    return 0;
+}
+
+void insertion_sort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++) 
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        // Move elements of arr[0..i-1], that are greater than key,
+        // to one position ahead of their current position
+        while (j >= 0 && arr[j] > key) 
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
